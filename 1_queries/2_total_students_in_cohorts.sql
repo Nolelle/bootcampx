b62b28634a76b0b0f559f415eed042b696b962b9
Edmund Yu <eyu1@ualberta.ca>
2021-10-11 22:32:14 +0000

    Add 3_students_no_phone_number.sql

--- 1_queries/2_total_students_in_cohorts.sql
+++ 1_queries/2_total_students_in_cohorts.sql
@@ -1,3 +1,4 @@
 SELECT COUNT(students) as count
 FROM students
-WHERE cohort_id = 1 OR cohort_id = 2 OR cohort_id = 3;
+WHERE cohort_id IN (1,2,3);
+-- WHERE cohort_id = 1 OR cohort_id = 2 OR cohort_id = 3;
