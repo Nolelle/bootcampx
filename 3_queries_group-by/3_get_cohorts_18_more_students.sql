524856b3ef63c66a3db393f6be6ae005b79ca6eb
Edmund Yu <eyu1@ualberta.ca>
2021-10-12 03:23:46 +0000

    Add 4_total_assignment_submissions_each_cohort.sql

--- 3_queries_group-by/3_get_cohorts_18_more_students.sql
+++ 3_queries_group-by/3_get_cohorts_18_more_students.sql
@@ -2,4 +2,4 @@ SELECT cohorts.name AS cohort_name, COUNT(students.*) AS student_count
 FROM cohorts JOIN students ON cohorts.id = cohort_id
 GROUP BY cohort_name
 HAVING COUNT(students.*) >= 18
-ORDER BY COUNT(students.*);
+ORDER BY COUNT(students.*)
